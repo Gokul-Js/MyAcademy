61ec8486d6fb3d7d394bb4362b4e5b6bbfa29f56
Gokul-Js <sharma.gokul258@gmail.com>
2022-08-27 19:27:31 +0530

    backend_initial_commit

---
+++ backend/models/User.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import validator from "validator"
+
+const schema = new mongoose.Schema({
+
+//    Name type, required
+name:{
+    type: String,
+    requiered:[true, "Please enter your Name"]
+},
+//    Email type, required, unique, validate
+email:{
+    type: String,
+    required:[true, "Please enter your Email"],
+    unique: true,
+    validate: validator.isEmail,
+},
+
+//    Password type, required, minLength, select
+password: {
+    type: String,
+    required:[true, "Please enter your Password"],
+    minLength: [6, "Password must be at least 6 Characters"],
+    select: false,
+},
+//    Role type, enum, default
+role: {
+    type: String,
+    enum: ["admin","user"],
+    default: "user",
+},
+//    Subscription id, status
+subscripton: {
+    id: String,
+    status: String,
+},
+//    Avatar public_id, url
+avatar: {
+    public_id: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    }
+},
+//    Playlist [ courseId,poster ]
+playlist: [
+    
+    {
+        course: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Course",
+        },
+    },
+],
+//    CreatedAt type, default
+//    ResetPasswordToken type
+//    ResetPasswordExpire type
+});
+
+export const User = mongoose.model("user", schema);
+
