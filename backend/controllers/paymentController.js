855117c810fbba1b87883d7c57b68cb3b6c0f238
Gokul-Js <sharma.gokul258@gmail.com>
2022-09-05 19:50:26 +0530

    Payment_subscription

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -2,6 +2,8 @@ import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { instance } from "../server.js";
 import {User} from "../models/User.js"
 import ErrorHandler from "../utils/errorHandler.js";
+import crypto from "crypto"
+import { Payment } from "../models/Payment.js";
 
 
 export const buySubscription = catchAsyncError(async(req,res,next) =>{
@@ -30,6 +32,62 @@ export const buySubscription = catchAsyncError(async(req,res,next) =>{
         subscriptionId: subscription.id,
       })
 
+})
 
+
+
+export const paymentVerification = catchAsyncError(async(req,res,next) =>{
+    
+    const {razorpay_signature, razorpay_payment_id, razorpay_subscription_id } = req.body
+
+    const user = await User.findById(req.user._id);
+
+    const subscription_id = user.subscription.id,
+
+     generated_signature = crypto
+    .createHmac('sha256', process.env.RAZORPAY_API_SECRET)
+    .update(razorpay_payment_id+"|"+subscription_id,"utf-8")
+    .digest("hex");
+
+    const isAuthentic = generated_signature === razorpay_signature
+
+    if(!isAuthentic) return res.redirect(`${process.env.FRONTEND_URL}/paymentfailed`)
+      
+     // database comes here
+    await Payment.create({
+        razorpay_signature,
+        razorpay_payment_id,
+        razorpay_subscription_id,
+    })
+
+    user.subscription.status="active"
+
+    await user.save()
+
+      res.redirect(`${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`)
+
+})
+
+export const getRazorPayKey = catchAsyncError(async(req,res,next)=>{
+    
+    res.status(200).json({
+        success: true,
+        key: process.env.RAZORPAY_API_KEY,
+    })
+})
+  
+export const cancelSubscription = catchAsyncError(async(req,res,next)=>{
+    const user = await User.findById(req.user._id)
+
+    const subscriptionId = user.subscription.id;
+    let refund = false;
+
+    await instance.subscription.cancel(subscriptionId)
+    
+
+    res.status(200).json({
+        success: true,
+        key: process.env.RAZORPAY_API_KEY,
+    })
 })
   
--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { buySubscription } from "../controllers/paymentController.js";
+import { buySubscription, getRazorPayKey, paymentVerification } from "../controllers/paymentController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 
@@ -7,5 +7,14 @@ const router = express.Router();
 
 // Buy Subscription
 router.route("/subscribe").get(isAuthenticated, buySubscription)
+
+// Verify Payment and save refrence in Database
+router.route("/paymentverification").post(isAuthenticated, paymentVerification);
+
+// Get RazorPay key
+router.route("/razorpaykey").get(getRazorPayKey)
+
+// Cancel Subscription
+router.route("/subscribe/cancel").delete(isAuthenticated, )
 
 export default router;
