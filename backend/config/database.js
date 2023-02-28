922ca63545c4f6aa68312797cfd5242bd20e9c3c
Ritesh Benjwal <69920964+riteshbenjwal@users.noreply.github.com>
2023-02-28 21:44:56 +0530

    Docker and nginx (#4)
    * backend dockerfile and compose setup
    
    * frontend dockerized

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async()=>{
-  const {connection} = await mongoose.connect(process.env.MANGO_URI);
+  const {connection} = await mongoose.connect(process.env.MONGO_URI);
   console.log(`Mongo Server is working on port: ${connection.host}`)
 }
