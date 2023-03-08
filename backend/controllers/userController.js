366749440aa1cf785d95250e4b2a794c0c0c34a6
riteshbenjwal <riteshbenjwal7@gmail.com>
2023-03-08 16:48:56 +0530

    admin stats flow implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,336 +1,314 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import ErrorHandler from "../utils/errorHandler.js";
-import {User} from "../models/User.js"
+import { User } from "../models/User.js";
 import { sendToken } from "../utils/sendToken.js";
 import { sendEmail } from "../utils/sendEmail.js";
-import crypto from "crypto"
-import {Course} from "../models/Course.js"
-import cloudinary from "cloudinary"
-import  getDataUri  from "../utils/dataUri.js"
+import crypto from "crypto";
+import { Course } from "../models/Course.js";
+import cloudinary from "cloudinary";
+import getDataUri from "../utils/dataUri.js";
 import { DESTRUCTION } from "dns";
+import { Stats } from "../models/Stats.js";
 
-export const register = catchAsyncError(async(req,res,next) =>{
-    const {name,email,password} = req.body;
-    const file = req.file;
+export const register = catchAsyncError(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const file = req.file;
 
-    if(!name || !email || !password || !file) 
-     return next(new ErrorHandler("Please Enter all Fields",400))
+  if (!name || !email || !password || !file)
+    return next(new ErrorHandler("Please Enter all Fields", 400));
 
-      let user = await User.findOne({ email });
+  let user = await User.findOne({ email });
 
-      if(user) return next(new ErrorHandler("user Already Exist",401));
+  if (user) return next(new ErrorHandler("user Already Exist", 401));
 
-      // upload file on Cloudinary
+  // upload file on Cloudinary
 
-      const fileUri = getDataUri(file)
-  
-      const mycloud = await cloudinary.v2.uploader.upload(fileUri.content)
+  const fileUri = getDataUri(file);
 
-      user = await User.create({
-        name,
-        email,
-        password,
-        avatar:{
-            public_id: mycloud.public_id,
-            url: mycloud.secure_url,
-        },
-      });
+  const mycloud = await cloudinary.v2.uploader.upload(fileUri.content);
 
-      sendToken(res,user,"Registered Successfully",201)
-      
-})
+  user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: mycloud.public_id,
+      url: mycloud.secure_url,
+    },
+  });
 
-export const login = catchAsyncError(async(req,res,next) =>{
-   
-    const {email,password} = req.body;
+  sendToken(res, user, "Registered Successfully", 201);
+});
 
-    if(!email || !password) 
-     return next(new ErrorHandler("Please Enter all Fields",400))
+export const login = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
 
-      const user = await User.findOne({ email }).select("+password");
+  if (!email || !password)
+    return next(new ErrorHandler("Please Enter all Fields", 400));
 
-      if(!user) return next(new ErrorHandler("Incorrect Email & Password",401));
+  const user = await User.findOne({ email }).select("+password");
 
-      const isMatch = await user.comparePassword(password)
+  if (!user) return next(new ErrorHandler("Incorrect Email & Password", 401));
 
-      if(!isMatch) return next(new ErrorHandler("Incorrect Email & Password",401))
- 
-      sendToken(res,user,`Welcome back, ${user.name}`, 200)
-      
-})
+  const isMatch = await user.comparePassword(password);
 
-export const logout = catchAsyncError(async(req,res,next)=>{
-    res
+  if (!isMatch)
+    return next(new ErrorHandler("Incorrect Email & Password", 401));
+
+  sendToken(res, user, `Welcome back, ${user.name}`, 200);
+});
+
+export const logout = catchAsyncError(async (req, res, next) => {
+  res
     .status(200)
-    .cookie("token",null, {
-        expires: new Date(Date.now()),
+    .cookie("token", null, {
+      expires: new Date(Date.now()),
     })
     .json({
-        success:true,
-        message:"Logged Out Successfully",
-    })
-})
+      success: true,
+      message: "Logged Out Successfully",
+    });
+});
 
-export const getMyProfile = catchAsyncError(async(req,res,next)=>{
-    const user = await User.findById(req.user._id);
-
-    res
-    .status(200)
-    .json({
-        success:true,
-        user,
-    })
-})
-
-export const changePassword = catchAsyncError(async(req,res,next)=>{
-
-    const {oldPassword,newPassword} = req.body;
-    if(!oldPassword || !newPassword) 
-    return next(new ErrorHandler("Please Enter all Fields",400))
-
-    const user = await User.findById(req.user._id).select("+password")
-
-    const isMatch = await user.comparePassword(oldPassword);
-
-    if(!isMatch) 
-    return next(new ErrorHandler("Incorrect Old Password",400));
-
-    user.password = newPassword;
-
-    await user.save()
-
-    res
-    .status(200)
-    .json({
-        success:true,
-        message:"Password Changed Successfully ",
-    })
-})
-
-export const updateProfile = catchAsyncError(async(req,res,next)=>{
-
-    const { name,email } = req.body;
-
-    const user = await User.findById(req.user._id);
-
-    if(name) user.name = name;
-    if(email) user.email = email;
-
-    await user.save()
-
-    res
-    .status(200)
-    .json({
-        success:true,
-        message:"Profile updated Successfully ",
-    })
-})
-
-export const updateProfilePicture = catchAsyncError(async(req,res,next)=>{
-   // during cloudinary
-   const file = req.file;
-
-   const user = await User.findById(req.user._id);
-
-   const fileUri = getDataUri(file);
-   const mycloud = await cloudinary.v2.uploader.upload(fileUri.content);
- 
-   await cloudinary.v2.uploader.destroy(user.avatar.public_id);
- 
-   user.avatar = {
-     public_id: mycloud.public_id,
-     url: mycloud.secure_url,
-   };
- 
-   await user.save();
-
-
-    res
-    .status(200)
-    .json({
-        success:true,
-        message:"Profile Picture updated Successfully ",
-    })
-
-})
-
-export const forgetPassword = catchAsyncError(async(req,res,next)=>{
-    const {email} = req.body;
-
-    const user = await User.findOne({ email });
-
-    if(!user) return next(new ErrorHandler("User Not Found",400));
-
-    const resetToken = await user.getResetToken();
-
-    await user.save();
-
-    const url = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
-
-    const message = `Click on the link to reset your password ${url}. If you have not request then Please Ignore`
-
-    // send token via email
-   await sendEmail(user.email,"MyAcademy Reset Password", message)
-
-
-     res
-     .status(200)
-     .json({
-         success:true,
-         message:`Reset Token has been sent to ${user.email}`,
-     })
- 
- })
-
- export const resetPassword = catchAsyncError(async(req,res,next)=>{
-
-    const {token} = req.params;
-    const resetPasswordToken = crypto
-       .createHash("sha256")  // we use crypto algorithams
-       .update(token)
-       .digest("hex");
-
-    const user = await User.findOne({
-        resetPasswordToken,
-        resetPasswordExpire: {
-            $gt: Date.now(),
-        },
-    })
-
-    if(!user) return next(new ErrorHandler("Token is invalid or has been expired",400));
-
-     user.password= req.body.password;
-     user.resetPasswordExpire=undefined;
-     user.resetPasswordToken=undefined;
-
-     await user.save()
-
-
-    res
-    .status(200)
-    .json({
-        success:true,
-        message:"Password Changed Successfully ",
-    })
-
-})
-
-export const addToPlaylist = catchAsyncError(async(req,res,next) => {
+export const getMyProfile = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user._id);
-  const course = await Course.findById(req.body._id)
-  if(!course) return next(new ErrorHandler("Invalid Course Id",404))
 
-  const itemExist = user.playlist.find((item)=>{
-    if(item.course.toString() === course._id.toString()) return true;
-  })
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
 
-  if(itemExist) return next(new ErrorHandler("Item Already Exit",409))
+export const changePassword = catchAsyncError(async (req, res, next) => {
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword)
+    return next(new ErrorHandler("Please Enter all Fields", 400));
+
+  const user = await User.findById(req.user._id).select("+password");
+
+  const isMatch = await user.comparePassword(oldPassword);
+
+  if (!isMatch) return next(new ErrorHandler("Incorrect Old Password", 400));
+
+  user.password = newPassword;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Password Changed Successfully ",
+  });
+});
+
+export const updateProfile = catchAsyncError(async (req, res, next) => {
+  const { name, email } = req.body;
+
+  const user = await User.findById(req.user._id);
+
+  if (name) user.name = name;
+  if (email) user.email = email;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Profile updated Successfully ",
+  });
+});
+
+export const updateProfilePicture = catchAsyncError(async (req, res, next) => {
+  // during cloudinary
+  const file = req.file;
+
+  const user = await User.findById(req.user._id);
+
+  const fileUri = getDataUri(file);
+  const mycloud = await cloudinary.v2.uploader.upload(fileUri.content);
+
+  await cloudinary.v2.uploader.destroy(user.avatar.public_id);
+
+  user.avatar = {
+    public_id: mycloud.public_id,
+    url: mycloud.secure_url,
+  };
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Profile Picture updated Successfully ",
+  });
+});
+
+export const forgetPassword = catchAsyncError(async (req, res, next) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) return next(new ErrorHandler("User Not Found", 400));
+
+  const resetToken = await user.getResetToken();
+
+  await user.save();
+
+  const url = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
+
+  const message = `Click on the link to reset your password ${url}. If you have not request then Please Ignore`;
+
+  // send token via email
+  await sendEmail(user.email, "MyAcademy Reset Password", message);
+
+  res.status(200).json({
+    success: true,
+    message: `Reset Token has been sent to ${user.email}`,
+  });
+});
+
+export const resetPassword = catchAsyncError(async (req, res, next) => {
+  const { token } = req.params;
+  const resetPasswordToken = crypto
+    .createHash("sha256") // we use crypto algorithams
+    .update(token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: {
+      $gt: Date.now(),
+    },
+  });
+
+  if (!user)
+    return next(new ErrorHandler("Token is invalid or has been expired", 400));
+
+  user.password = req.body.password;
+  user.resetPasswordExpire = undefined;
+  user.resetPasswordToken = undefined;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Password Changed Successfully ",
+  });
+});
+
+export const addToPlaylist = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+  const course = await Course.findById(req.body._id);
+  if (!course) return next(new ErrorHandler("Invalid Course Id", 404));
+
+  const itemExist = user.playlist.find((item) => {
+    if (item.course.toString() === course._id.toString()) return true;
+  });
+
+  if (itemExist) return next(new ErrorHandler("Item Already Exit", 409));
 
   user.playlist.push({
     course: course._id,
     poster: course.poster.url,
-  })
+  });
 
   await user.save();
 
-  res
-  .status(200)
-  .json({
-      success:true,
-      message:"Added to Playlist",
-  })
-})
+  res.status(200).json({
+    success: true,
+    message: "Added to Playlist",
+  });
+});
 
-export const removeFromPlaylist = catchAsyncError(async(req,res,next) => {
-    const user = await User.findById(req.user._id);
-  const course = await Course.findById(req.query.id)
-  if(!course) return next(new ErrorHandler("Invalid Course Id",404))
+export const removeFromPlaylist = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+  const course = await Course.findById(req.query.id);
+  if (!course) return next(new ErrorHandler("Invalid Course Id", 404));
 
   const newPlaylist = user.playlist.filter((item) => {
-    if (item.course.toString() !==course._id.toString()) return item;
-  })
-  
+    if (item.course.toString() !== course._id.toString()) return item;
+  });
+
   user.playlist = newPlaylist;
   await user.save();
 
-  res
-  .status(200)
-  .json({
-      success:true,
-      message:"Removed From Playlist",
-  })
-})
+  res.status(200).json({
+    success: true,
+    message: "Removed From Playlist",
+  });
+});
 
 // Admin Controllers
 
-export const getAllUsers = catchAsyncError(async(req,res,next) => {
-   
-    const users = await User.find({});
+export const getAllUsers = catchAsyncError(async (req, res, next) => {
+  const users = await User.find({});
+
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+export const updateUserRole = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) return next(new ErrorHandler("User not found", 404));
+
+  if (user.role === "user") user.role = "admin";
+  else user.role = "user";
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Role Updated",
+  });
+});
+
+export const deleteUser = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) return next(new ErrorHandler("User not found", 404));
+
+  await cloudinary.v2.uploader.destroy(user.avatar.public_id);
+
+  // cancel Subscription
+
+  await user.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "User Deleted Successfully",
+  });
+});
+
+export const deleteMyProfile = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+
+  await cloudinary.v2.uploader.destroy(user.avatar.public_id);
+
+  // cancel Subscription
+
+  await user.remove();
 
   res
-  .status(200)
-  .json({
-      success:true,
-      users,
-  })
-})
+    .status(200)
+    .cookie("token", null, {
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "User Deleted Successfully",
+    });
+});
 
-export const updateUserRole = catchAsyncError(async(req,res,next) => {
-   
-    const user = await User.findById(req.params.id);
+User.watch().on("change", async (data) => {
+  const stats = await Stats.find({}).sort({ createdAt: -1 }).limit(1);
 
-    if(!user) return next(new ErrorHandler("User not found", 404))
-    
-    if(user.role==="user") user.role="admin"
-    else user.role = "user";
-    
-    await user.save()
+  const subscriptions = await User.find({
+    "subscription.status": "active",
+  });
 
-  res
-  .status(200)
-  .json({
-      success:true,
-      message:"Role Updated"
-  })
-})
+  stats[0].subscription = subscriptions.length;
+  stats[0].users = await User.countDocuments();
+  stats[0].createdAt = new Date(Date.now());
 
-export const deleteUser = catchAsyncError(async(req,res,next) => {
-   
-    const user = await User.findById(req.params.id);
-
-    if(!user) return next(new ErrorHandler("User not found", 404))
-    
-    await cloudinary.v2.uploader.destroy(user.avatar.public_id)
-    
-    // cancel Subscription
-
-    await user.remove()
-
-  res
-  .status(200)
-  .json({
-      success:true,
-      message:"User Deleted Successfully"
-  })
-})
-
-export const deleteMyProfile = catchAsyncError(async(req,res,next) => {
-   
-    const user = await User.findById(req.user._id);
-    
-    await cloudinary.v2.uploader.destroy(user.avatar.public_id)
-    
-    // cancel Subscription
-
-    await user.remove()
-
-  res
-  .status(200)
-  .cookie("token",null, {
-    expires: new Date(Date.now()),
-  })
-  .json({
-      success:true,
-      message:"User Deleted Successfully"
-  })
-})
+  await stats[0].save();
+});
